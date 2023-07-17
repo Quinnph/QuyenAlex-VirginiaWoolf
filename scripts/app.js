91a007818d50172158859976647c1110f53a4e2e
Quyen Phan <quyenphan@Quyens-MacBook-Pro.fritz.box>
2023-07-17 09:56:30 +0200

    Add enableSubmitButton and resetModal functions, update eventlisteners

--- scripts/app.js
+++ scripts/app.js
@@ -3,33 +3,33 @@ let dataObject = []
 let currentIndex = 0
 const modalButton = document.querySelector('#modalButton')
 const submitButton = document.querySelector('.submit-btn')
+const backButton = document.querySelector('.btn-secondary')
 const modal = document.querySelectorAll('.modal')
 const modalBody = document.querySelector('.modal-body')
-
-let requiredInputs = document.querySelectorAll("[required]")
+const firstNameInputField = document.querySelector('#firstname')
+const lastNameInputField = document.querySelector('#lastname')
+const emailInputField = document.querySelector('#email')
 const messageInputField = document.querySelector('#message')
-let requiredInputFieldStates = {
-    "firstname": false,
-    "lastname": false,
-    "email": false,
-    "message": false
-}
-// TODO: enable submit(modal) button only if all fields are filled
-// messageInputField.addEventListener('input', enableSubmitButton)
-// function enableSubmitButton() {
-//     if (messageInputField.value.length <= 0) {
-//         submitButton.disabled = true;
-//     } else {
-//         submitButton.disabled = false;
-//     }
-// }
+
+firstNameInputField.addEventListener('input', enableSubmitButton)
+lastNameInputField.addEventListener('input', enableSubmitButton)
+emailInputField.addEventListener('input', enableSubmitButton)
+messageInputField.addEventListener('input', enableSubmitButton)
+
 modalButton.addEventListener('click', collectData)
 modalButton.addEventListener('click', printToModal)
-submitButton.addEventListener('click', reset)
+submitButton.addEventListener('click', resetInputFields)
 submitButton.addEventListener('click', closeModalSubmit)
+submitButton.addEventListener('click', resetModal)
+backButton.addEventListener('click', resetModal)
 
-// TODO: fix bug - clear modal after hit Send in case user send another form
-// TODO: fix bug - update dataObject and display in modal when user hit Back and edit input fields
+function enableSubmitButton() {
+     if (firstNameInputField.value.length <= 0 || lastNameInputField.value.length <= 0 || emailInputField.value.length <= 0 || messageInputField.value.length <= 0) {
+        modalButton.disabled = true;
+     } else {
+        modalButton.disabled = false;
+     }
+ }
 
 function collectData() {
     dataObject.push({
@@ -53,15 +53,20 @@ function printToModal() {
     })
 }
 
-function reset() {
+function resetInputFields() {
     contactForm.reset();
-    // $('#modal').on('hidden.bs.modal', function () {
-    //     $(".modal-body").html("");
-    //     console.log(modalBody);
-    // })
 }
 
 function closeModalSubmit() {
     alert('We will get back to you soon. Thank you!');
     $('#modal').modal('hide');
+    resetModal();
+    modalButton.disabled = true;
+}
+
+function resetModal(){
+    Object.entries(dataObject[currentIndex]).forEach(entry => {
+        modalBody.querySelector('p').remove();
+    })
+    dataObject = [];
 }
